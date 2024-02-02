d9ddcd1a2ce172fea2bed5ffc4cddd012000f2e2
rahulTiwari021 <31885581+rahulTiwari021@users.noreply.github.com>
2024-02-02 14:49:23 -0600

    Update index_dev.ts

--- src/index_dev.ts
+++ src/index_dev.ts
@@ -10,14 +10,23 @@ dotenv.config({
 
 import logger from "./utils/logger";
 import { PORT_NUMBER } from "./config";
-const  { router } = require("./services");
-
+const { router } = require("./services");
 
 // create express app
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.set('/', router);
+
+//app.set('/', router);
+// commented above statement to mount router on app and used app.use to listen to requests
+app.use('/', router);
+
+// Need to connect here to Database using TypeOrm with sync mode set to "true" so that it could create the entity in our local psql db
+/**
+ * Few Suggestions:
+ * it would be nice to have routes file in ts than js as it will ensure consitency accross the app. 
+ * And we can use commonjs import statements intead of using require
+ */
 
 app.listen(PORT_NUMBER);
 
